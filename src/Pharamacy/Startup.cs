b315f4337da76d68cd232f21463cf5fc0b68c4e8
Vahid <Vahid@DESKTOP-H31LQSR>
2019-09-11 18:19:51 +0430

    Add Project To remote Repository

--- src/Pharamacy/Startup.cs
+++ src/Pharamacy/Startup.cs
@@ -29,6 +29,7 @@ namespace Pharamacy
             // Adds all of the ASP.NET Core Identity related services and configurations at once.
             services.AddCustomIdentityServices();
 
+
             var siteSettings = services.GetSiteSettings();
             services.AddRequiredEfInternalServices(siteSettings); // It's added to access services from the dbcontext, remove it if you are using the normal `AddDbContext` and normal constructor dependency injection.
             services.AddDbContextPool<ApplicationDbContext>((serviceProvider, optionsBuilder) =>
